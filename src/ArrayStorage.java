485741967a4763e61e4d24c6cba4b03da109f5ba
Dim Sh <daemon75.sh@gmail.com>
2022-01-14 22:57:57 +0900

    test passed

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,28 +3,56 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int cursor = 0; //last-resume position in storage;
 
     void clear() {
+        if (cursor > 0) {
+            for (int i = 0; i < cursor; i++) {
+                storage[i] = null;
+            }
+            cursor = 0;
+        }
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        storage[cursor] = new Resume();
+        storage[cursor] = resume;
+        cursor++;
     }
 
     Resume get(String uuid) {
+        if (storage[0] != null) {
+            for (int i = 0; i < cursor; i++) {
+                if (storage[i].uuid.equals(uuid)) {
+                    return storage[i];
+                }
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < cursor; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = storage[i + 1];
+                if (storage[i] != null) {
+                    uuid = storage[i].uuid;
+                }
+            }
+        }
+        cursor--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] allResumes = new Resume[cursor];
+        for (int i = 0; i < cursor; i++) allResumes[i] = storage[i];
+        return allResumes;
     }
 
     int size() {
-        return 0;
+        return cursor;
     }
 }
--- src/MainArray.java
+++ src/MainArray.java
@@ -11,7 +11,7 @@ public class MainArray {
 
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        Resume r;
+        Resume resume;
         while (true) {
             System.out.print("Введите одну из команд - (list | size | save uuid | delete uuid | get uuid | clear | exit): ");
             String[] params = reader.readLine().trim().toLowerCase().split(" ");
@@ -31,9 +31,9 @@ public class MainArray {
                     System.out.println(ARRAY_STORAGE.size());
                     break;
                 case "save":
-                    r = new Resume();
-                    r.uuid = uuid;
-                    ARRAY_STORAGE.save(r);
+                    resume = new Resume();
+                    resume.uuid = uuid;
+                    ARRAY_STORAGE.save(resume);
                     printAll();
                     break;
                 case "delete":
