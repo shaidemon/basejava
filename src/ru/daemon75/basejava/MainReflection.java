687583d8953d2f9171cd94d4ffbaf391068694f7
Dim Sh <daemon75.sh@gmail.com>
2022-02-06 19:33:05 +0900

    Resume.toString via reflection

---
+++ src/ru/daemon75/basejava/MainReflection.java
@@ -0,0 +1,15 @@
+package ru.daemon75.basejava;
+
+import ru.daemon75.basejava.model.Resume;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
+        Resume resume = new Resume("test_resume");
+        Method method = resume.getClass().getMethod("toString");
+        System.out.println(method.invoke(resume, null));
+
+    }
+}
